0d34ad696aeba81b7b9f3b682c6a549e60e7e372
SupeRodv2 <supgeek.rod@gmail.com>
2016-09-14 00:59:54 +0800

    the charts.js

--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -1,11 +1,46 @@
 @extends('layouts.admin')
 
 @section('content')
+<script src="{{ asset('bower-assets/chart.js/dist/Chart.min.js') }}"></script>
 <div class="container">
     <div class="text-center">
-        <br><br><br><br>
-        <div class="h3 text-muted">Hey Community Cloud Platform dashboard</div>
-        <h2>Comming soon ...</h2>
+        <br><br>
+        <h2>The Trends</h2>
+        <br>
+
+        <canvas id="trend" width="400" height="140"></canvas>
+        <script>
+        var ctx = document.getElementById("trend");
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [1,2,3,4,5,6],
+                datasets: [{
+                    label: '# User',
+                    data: [12, 19, 3, 5, 12, 3],
+                    backgroundColor: "rgba(75,192,192,0.4)",
+                },
+                {
+                    label: '# Timeline',
+                    data: [1, 9, 33, 5, 2, 3],
+                    backgroundColor: "rgba(75,19,12,0.4)",
+                },
+                {
+                    label: '# Notice',
+                    data: [12, 39, 3, 5, 22, 3],
+                    backgroundColor: "rgba(7,192,19,0.4)",
+                }],
+                options: {
+                    scales: {
+                        xAxes: [{
+                            type: 'linear',
+                            position: 'bottom'
+                        }]
+                    }
+                },
+            }
+        });
+        </script>
     </div>
 </div>
 @endsection
