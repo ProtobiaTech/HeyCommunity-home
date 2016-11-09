aa6d34c4f0d5af39e66cfc5b37fc9447733462e2
SupeRodv2 <supgeek.rod@gmail.com>
2016-11-09 12:53:15 +0800

    fix some page

--- resources/views/home/open-sources.blade.php
+++ resources/views/home/open-sources.blade.php
@@ -7,17 +7,22 @@
   <br>
   <br>
   <div class="container">
-    <div style="max-width:500px; margin:0 auto;">
+    <div style="margin:0 auto;">
       <div class="text-center">
         <h2>开放源代码</h2>
         <p class="sub-heading">
-          HeyCommunity 开源版，遵循 GPL v3 开源许可协议 <br>
-          你可以自由地使用它，包括用于商业使用。你还可以基于它进行二次开发，进行修改、进行扩展做为自己的产品
+          HeyCommunity，遵循 GPLv3 开源许可协议 <br>
+          你可以自由地使用它，包括商业用途 <br>
+        </p>
+
+        <p class="sub-heading">
+          使用它为自己的社群快速构建一个 优美 / 独立 / 功能丰富的交流和互动平台 <br>
+          你还可以在它的基础上扩展新功能或者调整UI，这是非常简单的
         </p>
 
         <br>
         <p class="sub-heading">
-          开放源代码托管在 Github: <a href="https://github.com/dev4living/HeyCommunity" target="_blank">https://github.com/dev4living/HeyCommunity</a>
+          开放的源代码托管在 Github: <a href="https://github.com/dev4living/HeyCommunity" target="_blank">https://github.com/dev4living/HeyCommunity</a>
         </p>
       </div>
     </div>
